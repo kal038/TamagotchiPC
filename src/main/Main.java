346aad3e18bbc714838d8fef048ed4b66e60ea18
Khoi <kal038@bucknell.edu>
2020-04-06 20:12:38 -0400

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2020
+ * Instructor: Prof. Chris Dancy
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 4/6/20
+ * Time: 8:03 PM
+ *
+ * Project: BixbyFinalProject
+ * Package: main
+ * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package
+        main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is team Bixby");
+    }
+}
