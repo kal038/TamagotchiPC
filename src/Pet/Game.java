c5ede8b57999d0f9c71ac01e522e0899ca5f576e
Khoi <kal038@bucknell.edu>
2020-04-22 20:57:49 -0400

    Added a Game class, Added Food Enum, Added PetInteractions Class

---
+++ src/Pet/Game.java
@@ -0,0 +1,31 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2020
+ * Instructor: Prof. Chris Dancy
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 4/22/20
+ * Time: 8:54 PM
+ *
+ * Project: BixbyFinalProject
+ * Package: Pet
+ * Class: Game
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package
+        Pet;
+
+public class Game {
+    /*
+     * This class simulates an instance of a Game
+     */
+
+    public static void main(String[] args) {
+        PetMain myPet = new PetMain("Charizard");
+        myPet.increaseHappinessRate(50);
+    }
+}
